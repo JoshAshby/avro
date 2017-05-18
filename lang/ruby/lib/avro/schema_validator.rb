c8ac33367eab2dd3a0273f502ac3547c2e41a15f
JoshAshby <joshuaashby@joshashby.com>
2017-05-17 18:18:52 -0600

    remove loaded throw, rename full_errors, add some parenthesis
    small cleanup to remove unnecessary heavy/loaded throw which should give a tiiiiiiny performance bump
    rename full_errors to path_errors to better express what is stored
    add some parenthesis to make the style a bit more consistent
    move some methods and attributes into private methods to keep the API surface small

--- lang/ruby/lib/avro/schema_validator.rb
+++ lang/ruby/lib/avro/schema_validator.rb
@@ -25,7 +25,7 @@ module Avro
       end
 
       def to_s
-       result.to_s
+        result.to_s
       end
     end
 
@@ -33,9 +33,9 @@ module Avro
 
     ROOT_IDENTIFIER = '.'.freeze
     PATH_SEPARATOR = '.'.freeze
-    INT_RANGE = Schema::INT_MIN_VALUE..Schema::INT_MAX_VALUE
-    LONG_RANGE = Schema::LONG_MIN_VALUE..Schema::LONG_MAX_VALUE
-    COMPLEX_TYPES = [:array, :error, :map, :record, :request]
+    INT_RANGE = (Schema::INT_MIN_VALUE..Schema::INT_MAX_VALUE).freeze
+    LONG_RANGE = (Schema::LONG_MIN_VALUE..Schema::LONG_MAX_VALUE).freeze
+    COMPLEX_TYPES = [:array, :error, :map, :record, :request].freeze
 
     def self.validate *args
       new(*args).validate
@@ -45,20 +45,18 @@ module Avro
       new(*args).validate!
     end
 
-    attr_reader :root_identifier
-
     def initialize expected_schema, datum, root_identifier: ROOT_IDENTIFIER
-      @expected_schema  = expected_schema
-      @datum            = datum
+      @expected_schema = expected_schema
+      @datum           = datum
       @root_identifier = root_identifier
     end
 
-    def full_errors
-      @full_errors ||= Hash.new{ |hash, key| hash[ key ] = [] }
+    def path_errors
+      @path_errors ||= Hash.new{ |hash, key| hash[ key ] = [] }
     end
 
     def errors
-      full_errors.flat_map do |key, values|
+      path_errors.flat_map do |key, values|
         values.map do |value|
           "at #{ key } #{ value }"
         end
@@ -66,21 +64,15 @@ module Avro
     end
 
     def add_error(path, message)
-      full_errors[path] << message
+      path_errors[path] << message
     end
 
     def failure?
-      full_errors.any?
+      path_errors.any?
     end
 
     def successful?
-      full_errors.empty?
-    end
-
-    def merge_errors other_errors
-      other_errors.each do |key, values|
-        full_errors[ key ].concat values
-      end
+      path_errors.empty?
     end
 
     def to_s
@@ -94,40 +86,45 @@ module Avro
 
     def validate!
       return self if validate.successful?
-
       fail ValidationError, self
     end
 
     private
 
-    def validate_recursive(expected_schema, datum, path)
-      type_mismatch = :type_mismatch
+    attr_reader :root_identifier
 
-      res = catch(:halt) do |tag|
+    def merge_errors(other_errors)
+      other_errors.each do |key, values|
+        path_errors[ key ].concat(values)
+      end
+    end
+
+    def validate_recursive(expected_schema, datum, path)
+      catch(:halt) do |tag|
         case expected_schema.type_sym
         when :null
-          throw tag, type_mismatch unless datum.nil?
+          throw tag unless datum.nil?
 
         when :boolean
-          throw tag, type_mismatch unless [true, false].include?(datum)
+          throw tag unless [true, false].include?(datum)
 
         when :string, :bytes
-          throw tag, type_mismatch unless datum.is_a?(String)
+          throw tag unless datum.is_a?(String)
 
         when :int
-          throw tag, type_mismatch unless datum.is_a?(Integer)
+          throw tag unless datum.is_a?(Integer)
 
           add_error(path, "out of bound value #{datum}") unless INT_RANGE.cover?(datum)
           return
 
         when :long
-          throw tag, type_mismatch unless datum.is_a?(Integer)
+          throw tag unless datum.is_a?(Integer)
 
           add_error(path, "out of bound value #{datum}") unless LONG_RANGE.cover?(datum)
           return
 
         when :float, :double
-          throw tag, type_mismatch unless [Float, Integer].any?(&datum.method(:is_a?))
+          throw tag unless [Float, Integer].any?(&datum.method(:is_a?))
 
         when :fixed
           if datum.is_a? String
@@ -164,11 +161,11 @@ module Avro
         else
           fail "Unexpected schema type #{expected_schema.type_sym} #{expected_schema.inspect}"
         end
+
+        return
       end
 
-      if res == type_mismatch
-        add_error(path, "expected type #{expected_schema.type_sym}, got #{actual_value_message(datum)}")
-      end
+      add_error(path, "expected type #{expected_schema.type_sym}, got #{actual_value_message(datum)}")
     end
 
     def validate_hash(expected_schema, datum, path)
@@ -220,7 +217,7 @@ module Avro
       complex_type_failed = failures.find { |r| COMPLEX_TYPES.include?(r[:type]) }
 
       if complex_type_failed
-        merge_errors complex_type_failed[:result].full_errors
+        merge_errors complex_type_failed[:result].path_errors
       else
         types = expected_schema.schemas.map { |s| "'#{s.type_sym}'" }.join(', ')
         add_error(path, "expected union of [#{types}], got #{actual_value_message(datum)}")
@@ -229,7 +226,10 @@ module Avro
 
     def validate_possible_types(datum, expected_schema, path)
       expected_schema.schemas.map do |schema|
-        { type: schema.type_sym, result: self.class.validate(schema, datum, root_identifier: path) }
+        {
+          type: schema.type_sym,
+          result: self.class.validate(schema, datum, root_identifier: path)
+        }
       end
     end
 
@@ -244,11 +244,9 @@ module Avro
                     ruby_to_avro_type(value.class)
                   end
 
-      if value.nil?
-        avro_type
-      else
-        "#{avro_type} with value #{value.inspect}"
-      end
+      return avro_type if value.nil?
+
+      "#{avro_type} with value #{value.inspect}"
     end
 
     def ruby_to_avro_type(ruby_class)
